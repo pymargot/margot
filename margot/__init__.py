9a8ebdf64b464f8d7e9131dcb0161c79076a8e9a
Rich Atkinson <rich@airteam.com.au>
2020-06-30 11:57:26 +1000

    example docstring

--- margot/__init__.py
+++ margot/__init__.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
 from margot.data.frames import MargotDataFrame
 from margot.data.symbols import Symbol
 from margot.data.ratio import Ratio
